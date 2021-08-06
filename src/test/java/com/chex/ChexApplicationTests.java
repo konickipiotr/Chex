1ea94ca034a30cebd45cccabf6dc3bbeabf9a87f
Piotr Konicki <konicki.piotr@gmail.com>
2021-08-06 20:00:29 +0200

    Start of the project

---
+++ src/test/java/com/chex/ChexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
