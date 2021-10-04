e220425433f84cef57bf29485ae4fbbc8605a4e6
Piotr Konicki <konicki.piotr@gmail.com>
2021-10-05 00:05:33 +0200

    additional prints

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+echo 'pre'
 ps axf | grep 'java -jar Chex-0.0.1-SNAPSHOT.jar' | grep -v grep | sudo awk '{print "sudo kill -9 " $1}' | sh
+echo 'mid'
 java -jar target/Chex-0.0.1-SNAPSHOT.jar &
+echo 'post'
